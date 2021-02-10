eba7d6922f193c09c93f5296d3e5f8bdc5eef0ee
Grant Williams <grant.williams2986@gmail.com>
2021-02-10 10:51:32 +0100

    Start betting before others maybe

--- Player.js
+++ Player.js
@@ -1,13 +1,28 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return "0.1";
+  }
+
+  static get name() {
+    return 'Go Fish'
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    // bet(0);
+    const { current_buy_in, in_action, players } = gameState;
+
+    const betAmount = current_buy_in - players[in_action].bet;
+    bet(betAmount > 50 ? 0 : betAmount);
   }
 
-  static showdown(gameState) {
+  static showdown(gameState) {}
+
+  static readCards(gameState) {
+    const { hole_cards } = gameState.players.find(
+      ({ name }) => name
+    );
+
+    return hole_cards;
   }
 }
 
